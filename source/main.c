19ea84ffbac9223c833e727b42e296671a7c093b
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2021-04-06 21:56:44 +0200

    color choice
    added color choice

--- source/main.c
+++ source/main.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <3ds.h>
-#define infoNum 8
+#define infoNum 9
 #define strLen 30
 
 char info[infoNum][strLen];
@@ -13,7 +13,7 @@ int hours, minutes, pmin=-1;
 void getInfo()
 {
 	FILE *fp;
-	int i=0, k=0;
+	int i=0;
 
 	fp = fopen("info.ini", "r");
 	rewind(fp);
@@ -25,10 +25,16 @@ void getInfo()
 
     for(i=0;i<infoNum;i++)
     {
-        for(k=0;k<strLen;k++)
+        if(info[i][strlen(info[i])-1] == 10)
+            info[i][strlen(info[i])-1] = 0;
+    }
+
+    for(i=0;i<infoNum;i++)
+    {
+        char *ptr = strchr(info[i], '\r');
+        if (ptr)
         {
-            if(info[i][k]=='\n')
-                info[i][k]='\0';
+            *ptr  = '\0';
         }
     }
 
@@ -48,23 +54,54 @@ void getTime()
 
 void printLogo()
 {
-	printf("       ######       \033[1;31m%s\033[0;m\n",info[0]);
-	printf("     ####  ####     \n");
-	printf("    #####  #####    -----------\n");
-	printf("   ## ##    ## ##   \n");
-	printf("   ##          ##   \033[1;31mOS\033[0;m: %s\n", info[1]);
-	printf("  ####        ####  \n");
-	printf("  ####  ####  ####  \033[1;31mFirmware\033[0;m: %s\n", info[2]);
+    setvbuf(stdout, NULL, _IONBF, 0); 
+	printf("       ######       %s",info[0]);
+    							printf("%s\033[0;m\n",info[1]);
+                                printf("     ####  ####     \n");
+    printf("    #####  #####    -----------\n");
+    printf("   ## ##    ## ##   \n");
+    printf("   ##          ##   %sOS",info[0]);
+    							printf("\033[0;m: %s\n", info[2]);
+                                printf("  ####        ####  \n");
+	printf("  ####  ####  ####  %sFirmware",info[0]);
+								printf("\033[0;m: %s\n", info[3]);
 	printf("  #### ###### ####  \n");
-	printf("  #### ###### ####  \033[1;31mHost\033[0;m: %s\n", info[3]);
+	printf("  #### ###### ####  %sHost",info[0]);
+								printf("\033[0;m: %s\n", info[4]);
 	printf("  ###  ######  ###  \n");
-	printf("  #     ####     #  \033[1;31mResolution\033[0;m: %s\n", info[4]);
+	printf("  #     ####     #  %sResolution",info[0]);
+								printf("\033[0;m: %s\n", info[5]);
 	printf("  #  ##########  #  \n");
-	printf("  ####  #  #  ####  \033[1;31mCPU\033[0;m: %s\n", info[5]);
+	printf("  ####  #  #  ####  %sCPU",info[0]);
+								printf("\033[0;m: %s\n", info[6]);
 	printf("   ##   #  #   ##   \n");
-	printf("    #          #    \033[1;31mGPU\033[0;m: %s\n", info[6]);
+	printf("    #          #    %sGPU",info[0]);
+								printf("\033[0;m: %s\n", info[7]);
 	printf("    ##        ##    \n");
-	printf("     ##########     \033[1;31mMemory\033[0;m: %s\n", info[7]);
+	printf("     ##########     %sMemory",info[0]);
+								printf("\033[0;m: %s\n", info[8]);
+}
+
+void getColor()
+{
+	if(strcmp(info[0],"red")==0)
+		strcpy(info[0],"\033[1;31m");
+	else if(strcmp(info[0],"green")==0)
+		strcpy(info[0],"\033[1;32m");
+	else if(strcmp(info[0],"yellow")==0)
+		strcpy(info[0],"\033[1;33m");
+	else if(strcmp(info[0],"blue")==0)
+		strcpy(info[0],"\033[1;34m");
+	else if(strcmp(info[0],"magenta")==0)
+		strcpy(info[0],"\033[1;35m");
+	else if(strcmp(info[0],"cyan")==0)
+		strcpy(info[0],"\033[1;36m");
+	else if(strcmp(info[0],"white")==0)
+		strcpy(info[0],"\033[1;37m");
+	else
+		strcpy(info[0],"\033[1;31m");
+
+    fflush(NULL);
 }
 
 int timeCheck()
@@ -96,6 +133,7 @@ int main(int argc, char* argv[])
 			printf("                     - %02d:%02d -", hours, minutes); 
 			printf("\n\n\n\n\n");
 			getInfo();
+			getColor();
 			printLogo();
 		}
 
