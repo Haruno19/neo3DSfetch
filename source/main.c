2c96ccefe2286cdee8237de869fd9f701c4f90e7
Stefano Gini <61376940+Haruno19@users.noreply.github.com>
2021-04-06 16:13:57 +0200

    added time
    added time displaying that refreshes every minute

--- source/main.c
+++ source/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 #include <3ds.h>
 #define infoNum 8
 #define strLen 30
 
 char info[infoNum][strLen];
+int hours, minutes, pmin=-1;
 
 void getInfo()
 {
@@ -32,6 +35,17 @@ void getInfo()
 	fclose(fp);
 }
 
+void getTime()
+{
+	time_t now;
+ 
+    time(&now);
+    struct tm *local = localtime(&now);
+ 
+    hours = local->tm_hour;         
+    minutes = local->tm_min;   
+}
+
 void printLogo()
 {
 	printf("       ######       \033[1;31m%s\033[0;m\n",info[0]);
@@ -53,20 +67,37 @@ void printLogo()
 	printf("     ##########     \033[1;31mMemory\033[0;m: %s\n", info[7]);
 }
 
+int timeCheck()
+{
+	if(pmin!=minutes)
+	{
+		pmin=minutes;
+		return 1;
+	}
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
 	gfxInitDefault();
 	consoleInit(GFX_TOP, NULL);
-
-	printf("\n\n\n\n\n\n");
-	getInfo();
-	printLogo();
 
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		gfxSwapBuffers();
 		hidScanInput();
+
+		getTime();
+		if(timeCheck())
+		{
+			iprintf("\x1b[2J");
+			printf("\n\n");
+			printf("                     - %02d:%02d -", hours, minutes); 
+			printf("\n\n\n\n\n");
+			getInfo();
+			printLogo();
+		}
 
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
